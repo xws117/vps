a52f33138d71d5cb50bc12cd57ca40e95322c7ae
xws117 <1179066213@qq.com>
2017-09-18 18:07:18 +0800

    maxiaolei's picture

---
+++ src/main/java/hello/Hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+	@RequestMapping("hello")
+	public String sayhello(){
+		return "hello";
+	} 
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h2>This is XWS's WebSite<h2>
+<img src="images/1.png"/>
+</body>
+</html>
